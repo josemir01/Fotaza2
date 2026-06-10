91f65d0658cbc88ec4b865d63c68da168978d239
josemir01 <josemirzaleh45@gmail.com>
2026-06-10 19:57:18 -0300

    ssl para render

--- db/config.js
+++ db/config.js
@@ -3,13 +3,19 @@ import { Sequelize } from 'sequelize';
 
 console.log(process.env.DB_PASSWORD)
 
-const sequelize=new Sequelize({
-dialect:'postgres',
-host:process.env.DB_HOST,
-username:process.env.DB_USER,
-database:process.env.DB_NAME,
-password:process.env.DB_PASSWORD,
-port:process.env.DB_PORT,
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Esto es obligatorio para que Render y Neon se entiendan sin problemas de certificados
+        }
+    }
 })
 
 export default sequelize
